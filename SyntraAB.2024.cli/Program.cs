fc125bbde02360c8e6574e3ea6644d8447951b42
Filip Geens <filip.geens@fleximinded.be>
2024-09-10 20:43:46 +0200

    Add project files.

---
+++ SyntraAB.2024.cli/Program.cs
@@ -0,0 +1,13 @@
+﻿using Fleximinded.Core.Parts.CLI;
+
+
+namespace SyntraAB._2024.cli
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CliRuntime runtime = new();
+        }
+    }
+}
